659eaf9a80554886447d7a5484c2a759a6e6d486
Malith Dulsara <malithdulsara2001@gmail.com>
2026-01-29 15:49:48 +0530

    use @requestbody in usercontrolle adduser method

--- src/main/java/edu/icet/controller/UserController.java
+++ src/main/java/edu/icet/controller/UserController.java
@@ -3,10 +3,7 @@ package edu.icet.controller;
 import edu.icet.dto.UserDto;
 import edu.icet.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,7 +14,7 @@ public class UserController {
    private final UserService service;
 
     @PostMapping("/add")
-    public void addUser(UserDto userDto) {
+    public void addUser(@RequestBody UserDto userDto) {
         service.saveUser(userDto);
     }
 
